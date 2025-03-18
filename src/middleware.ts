e0851da016847bf7fbe71696c648755449c531aa
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-03-18 11:20:16 +0700

    fix: middleware for case /* path

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,15 +18,18 @@ export function middleware(req: NextRequest) {
 
     // Case 1: Authenticated user trying to access blocked pages
     if (isLoggedIn && blockedPagesAfterAuth.includes(pathname)) {
-        return NextResponse.redirect(new URL('/', req.url)); // Redirect to home or dashboard
+        return NextResponse.redirect(new URL('/', req.url));
     }
 
     // Case 2: Unauthenticated user trying to access protected pages
-    if (!isLoggedIn && !publicPages.includes(pathname) && !blockedPagesAfterAuth.includes(pathname)) {
+    const isPublicPage =
+        publicPages.includes(pathname) ||
+        publicPages.some((page) => new RegExp(`^${page.replace('*', '.*')}$`).test(pathname));
+
+    if (!isLoggedIn && !isPublicPage && !blockedPagesAfterAuth.includes(pathname)) {
         return NextResponse.redirect(new URL(`/sign-in?redirect=${pathname}`, req.url));
     }
 
-    // Allow request to proceed if none of the above conditions are met
     return NextResponse.next();
 }
 
