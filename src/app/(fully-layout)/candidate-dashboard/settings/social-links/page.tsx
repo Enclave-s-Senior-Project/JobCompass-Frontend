a5274a474a961fbab896e01661ed85a32c584b3e
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-03-26 16:22:06 +0700

    chore: do skeleton for social links while loading

--- src/app/(fully-layout)/candidate-dashboard/settings/social-links/page.tsx
+++ src/app/(fully-layout)/candidate-dashboard/settings/social-links/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function SocialLinkPageForCandidate() {
     return (
-        <div>
+        <div className="min-h-96">
             <FormSocialLinks useType="candidate" />
         </div>
     );
