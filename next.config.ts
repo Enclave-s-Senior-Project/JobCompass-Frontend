b536a7894be701b6be744d3310ff455b62015959
Hoaian03IT <dangtranhoaian8603@gmail.com>
2025-04-15 23:59:56 +0700

    chore: allow images from all hostname

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
-import { basePathPublicStaticFiles } from '@/middleware';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.pinimg.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'job-compass-store.s3.ap-southeast-1.amazonaws.com',
-            },
+            {protocol: 'https', hostname: '**'}
         ],
     },
     output: 'standalone',
