e59d0626c3873b8395bca070fbcc73fbd2f290e9
hoaian03IT <dangtranhoaian8603@gmail.com>
2025-05-21 16:36:54 +0700

    chore: update personalized chat

--- src/components/custom-ui/local/chatbox-popup.tsx
+++ src/components/custom-ui/local/chatbox-popup.tsx
@@ -5,11 +5,8 @@ import Image from 'next/image';
 
 const ChatbotPopup = memo(() => {
     return (
-        <Popover>
+        <Popover open>
             <PopoverTrigger asChild>
-                {/* <Button variant="primary" size="icon-lg" className="rounded-full">
-                    <Bot />
-                </Button> */}
                 <button className="size-14 overflow-hidden rounded-full shadow-lg drop-shadow-sm">
                     <Image
                         className="h-full w-full p-0"
@@ -20,7 +17,7 @@ const ChatbotPopup = memo(() => {
                     />
                 </button>
             </PopoverTrigger>
-            <PopoverContent className="w-full max-w-96 border-0 p-0 shadow-none" align="end" side="left">
+            <PopoverContent className="w-full max-w-[480px] border-0 p-0 shadow-none" align="end" side="left">
                 <ChatBox />
             </PopoverContent>
         </Popover>
